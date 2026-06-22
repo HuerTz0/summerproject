7386ab56d95fcf955f1650eba4bca6b809adfa57
HuerTz0 <shouvikdev07@gmail.com>
2026-06-22 17:06:42 -0500

    pogoman v2

--- firstproject/Scripts/pogoman.gd
+++ firstproject/Scripts/pogoman.gd
@@ -1,35 +1,47 @@
-extends RigidBody2D
-#Phys Variables
+extends Node2D
 
-@export var torque_power: float = 15000.0
 
-#Jump Variables
-@export var min_jump_imp: float = 300.0;
-@export var max_jump_imp: float = 1200.0;
-@export var charge_up: float = 1500.0;
+# --- NODES ---
+@onready var main_body: RigidBody2D = $MainBody
+@onready var ground_check: RayCast2D = $GroundCheck
+@onready var pogo_tip: RigidBody2D = $PogoTip
 
-var current_charge: float = 0.0;
-@onready var pogo_tip: RayCast2D = $pogoTip;
+# --- FORCES ---
+@export var torque_power: float = 25000.0 # Increased because MainBody is heavier now
+@export var min_jump_impulse: float = 1000.0
+@export var max_jump_impulse: float = 4000.0
+@export var charge_speed: float = 5000.0
+
+var current_charge: float = 0.0
 
 func _physics_process(delta: float) -> void:
+	# Keep the ground check RayCast aligned with the pogo stick
+	ground_check.global_position = main_body.global_position
+	ground_check.target_position = Vector2.DOWN.rotated(main_body.rotation) * 50 # Adjust length to reach past the tip
+	
+	pogo_tip.rotation = main_body.rotation
+	
 	_handle_rotation()
 	_handle_jumping(delta)
 
-
 func _handle_rotation() -> void:
 	var tilt = Input.get_axis("lean_left", "lean_right")
+	
 	if tilt != 0:
-		apply_torque(tilt*torque_power)
+		# Apply twisting force ONLY to the heavy main body
+		main_body.apply_torque(tilt * torque_power)
 
 func _handle_jumping(delta: float) -> void:
-	if pogo_tip.is_colliding():
+	if ground_check.is_colliding():
 		if Input.is_action_pressed("jump"):
-			current_charge += charge_up*delta
-			current_charge = clamp(current_charge, min_jump_imp, max_jump_imp)
+			current_charge += charge_speed * delta
+			current_charge = clamp(current_charge, min_jump_impulse, max_jump_impulse)
+			
 		elif Input.is_action_just_released("jump"):
-			var jump_direction = Vector2.UP.rotated(rotation)
-			apply_central_impulse(jump_direction*current_charge)
+			# Launch the main body upwards
+			var jump_direction = Vector2.UP.rotated(main_body.rotation)
+			main_body.apply_central_impulse(jump_direction * current_charge)
 			current_charge = 0.0
+	else:
 		if not Input.is_action_pressed("jump"):
 			current_charge = 0.0
-			
